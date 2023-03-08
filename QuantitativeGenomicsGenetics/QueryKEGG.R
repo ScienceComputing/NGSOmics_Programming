759d7587a7c378c12e8fac1e54e7ff06e2947de7
Angela <90419301+lancome1@users.noreply.github.com>
2023-03-07 20:27:17 -0500

    Update QueryKEGG.R

--- QuantitativeGenomicsGenetics/QueryKEGG.R
+++ QuantitativeGenomicsGenetics/QueryKEGG.R
@@ -14,7 +14,38 @@ keggFind("drug", "immune")
 
 # Get the identifiers of the genes/drugs
 keggFind("genes", "immune") |> names()
+immune.gene.id <- keggFind("drug", "immune") |> names()
 keggFind("drug", "immune") |> names()
 
 # Find all ligands that interact with ATP
 keggFind("ligand", "ATP")
+
+# Find immune-related pathways
+path.id <- keggFind("pathway", "immune") |> names()
+
+# Check the genes involving in each immune-related pathway in **humans (hsa)**
+for (id.i in path.id){
+  id.new <- gsub("path:map", "hsa", id.i)
+  res <- keggGet(id.new)
+  cat("Pathway Name", res[[1]]$NAME, "\n") 
+  print(res[[1]]$GENE[1:10])
+}
+
+# Find more details on immune-related genes
+immune.gene <- keggGet(immune.gene.id)
+
+# Let us see more information stored in the `immune.gene` object
+names(immune.gene[[1]])
+# [1] "ENTRY"       "NAME"        "PRODUCT"     "FORMULA"    
+# [5] "EXACT_MASS"  "MOL_WEIGHT"  "SEQUENCE"    "SOURCE"     
+# [9] "CLASS"       "REMARK"      "EFFICACY"    "COMMENT"    
+# [13] "TARGET"      "METABOLISM"  "INTERACTION" "STR_MAP"    
+# [17] "OTHER_MAP"   "BRITE"       "DBLINKS"     "ATOM"       
+# [21] "BOND"  
+
+# View the sequence of the first immune-related gene
+# keggGet(dbentries = immune.gene.id[1], option = "sequence")
+immune.gene[[1]]$SEQUENCE
+
+# Additional info: https://www.bioconductor.org/packages/devel/bioc/vignettes/KEGGREST/inst/doc/KEGGREST-vignette.html
+
