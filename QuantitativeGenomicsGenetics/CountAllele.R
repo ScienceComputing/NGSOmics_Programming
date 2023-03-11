e53532eb71c7bc45f1e419ed2cde161846da6fe9
Anni <90419301+anniliu9@users.noreply.github.com>
2023-03-11 10:28:35 -0500

    Update CountAllele.R

--- QuantitativeGenomicsGenetics/CountAllele.R
+++ QuantitativeGenomicsGenetics/CountAllele.R
@@ -57,3 +57,53 @@ xa_matrix <- xa_matrix - 1 # Center on 0
 xa_matrix[1:10, 1:10]
 
 
+# Write an expanded function that handle diverse situations in the class and dim of the imported data, the frequencies of minor and major alleles, the threshold of the minor allele frequency, and dummary variables in the additive genetic model
+xa_converter <- function(geno_in, maf_limit = 0.01, return_minor_allele = F){
+  # Combine a pair of two nucleotide positions to each genotype which is a vector of double letter elements: AA, AG, GG, ...
+  if(class(geno_in) == "matrix" | class(geno_in) == "data.frame"){
+    if(ncol(geno_in) > 1){
+      geno <- paste0(geno_in[, 1], geno_in[, 2])
+    } else {
+      geno <- as.vector(geno_in)
+    }
+  } else {
+    geno <- geno_in
+  }
+  
+  return_coding <- rep(NA, length(geno))
+  
+  # Determine the minor and major alleles
+  geno_undone <- unlist(strsplit(paste0(geno_in[, 1], geno_in[, 2]), ''))
+  geno_count <- table(geno_undone)
+  maf <- min(geno_count)[1]/length(geno) # maf: minor allele frequency
+  if(length(geno_count) == 1 | maf < maf_limit){
+   return(return_coding) 
+  } else if(geno_count[1] == geno_count[2]){
+    minor_allele <- sample(names(geno_count), 1)
+    major_allele <- names(geno_count)[names(geno_count) != minor_allele]
+  } else {
+    minor_allele <- names(geno_count[geno_count == min(geno_count)])
+    major_allele <- names(geno_count[geno_count == max(geno_count)])
+  }
+
+  # Categorize genotypes to the "dummy variables" centered on 0 (for additive genetic model)
+  # The additive genetic model assumes that the effects of two alleles at a given locus (location on a chromosome) are additive, meaning that the more copies of the "good" allele a person has, the better their trait value will be.
+  return_coding[geno == paste0(minor_allele, minor_allele)] <- 1
+  return_coding[geno == paste0(minor_allele, major_allele) | geno == paste0(major_allele, minor_allele) ] <- 0
+  return_coding[geno == paste0(major_allele, major_allele)] <- -1
+  
+  if(return_minor_allele){
+    return(list(return_coding, minor_allele))
+  } else {
+    return(return_coding)
+  }
+}
+
+xa_matrix <- matrix(NA, nrow = nrow(geno_import), ncol = ncol(geno_import)/2)
+
+for (i in 1:(ncol(geno_import)/2)){
+    xa_matrix[, i] <- xa_converter(geno_import[,c(2*i - 1, 2*i)]) # Grab pairs of columns; (1,2), (3,4)...(399,400); Each column of xa_matrix is the returned vector `return_coding`, which is an integer vector taking the value 1, 0, or -1 of the sample size length   
+}
+
+xa_matrix[1:10, 1:10]
+
