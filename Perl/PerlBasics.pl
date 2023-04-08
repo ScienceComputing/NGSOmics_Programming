40b018789cb7c594dc827bda014ad3e463cd8afb
Anni <90419301+anniliu9@users.noreply.github.com>
2023-04-08 11:12:41 -0400

    Update PerlBasics.pl

--- Perl/PerlBasics.pl
+++ Perl/PerlBasics.pl
@@ -1,10 +1,30 @@
 $var1=10;
 $var2=18;
+print "The var1 is: $var1, \n";
+print "The var2 is: $var2, \n";
 $product=$var1*$var2;
-print "$product, \n";
+print "The product of var1 and var2 is: $product, \n";
 $sum=$var1+$var2;
-print "$sum, \n";
+print "The sum of var1 and var2 is: $sum, \n";
 $remainder=$var1%$var2;
-print "$remainder\n";
+print "The remainder of dividing var1 by var2 is: $remainder, \n";
+
+$var1+=9;
+print "The new var1 is: $var1, \n";
+$var1=10;
+$var1-=9;
+print "The new var1 is: $var1, \n";
+$var1=10;
+$var1*=9;
+print "The new var1 is: $var1, \n";
+$var1=10;
+$var1/=9;
+print "The new var1 is: $var1, \n";
+$var1=10;
+$var1=$var1**2;
+print "The new var1 is: $var1, \n";
+
+$string="ATC"x3;
+print "The sequence is: $string, \n";
 
 # perl PerlBasics.pl
