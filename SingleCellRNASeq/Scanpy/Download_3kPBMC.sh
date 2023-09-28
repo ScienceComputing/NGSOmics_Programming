8d0cb1a922ff2a18c3177306d9be46a7b48f5e27
Anni <90419301+anniliu7@users.noreply.github.com>
2023-09-28 07:38:26 -0400

    Create Download_3kPBMC.sh

---
+++ SingleCellRNASeq/Scanpy/Download_3kPBMC.sh
@@ -0,0 +1,3 @@
+wget http://cf.10xgenomics.com/samples/cell-exp/1.1.0/pbmc3k/pbmc3k_filtered_gene_bc_matrices.tar.gz -O pbmc3k_filtered_gene_bc_matrices.tar.gz
+tar -xzf pbmc3k_filtered_gene_bc_matrices.tar.gz
+mkdir preprocessed_data
