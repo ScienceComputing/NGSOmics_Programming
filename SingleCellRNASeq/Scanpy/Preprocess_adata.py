699994efaeef409751be49fe7f020a960d55e150
Anni <90419301+anniliu7@users.noreply.github.com>
2023-09-29 06:47:23 -0400

    Update Preprocess_adata.py

--- SingleCellRNASeq/Scanpy/Preprocess_adata.py
+++ SingleCellRNASeq/Scanpy/Preprocess_adata.py
@@ -1,3 +1,5 @@
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 import scanpy as sc
 import anndata as ad
 import numpy as np
@@ -8,3 +10,87 @@ adata_pbmc = ad.read_h5ad(outcome_path)
 
 # Display the genes that contribute the largest proportion of counts within each individual cell, spanning all the cells.
 sc.pl.highest_expr_genes(adata_pbmc, n_top=30)
+
+# Let's perform the basic quality control
+sc.pp.filter_cells(adata_pbmc, min_genes=200) # We set 200 as the minimum number of genes expressed required for a barcode to pass filtering
+sc.pp.filter_genes(adata_pbmc, min_cells=3) # We set 3 as the minimum number of barcodes expressed required for a gene to pass filtering
+
+# Compute the distribution of mitochondrial genes
+adata_pbmc.var['mt'] = adata_pbmc.var_names.str.startswith('MT-') # adata_pbmc = sc.read_10x_mtx(..., var_names='gene_symbols', ...)
+sc.pp.calculate_qc_metrics(adata_pbmc, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+adata_pbmc.var
+#                       gene_ids  n_cells     mt  n_cells_by_counts  mean_counts  pct_dropout_by_counts  total_counts
+# AL627309.1     ENSG00000237683        9  False                  9     0.003333              99.666667           9.0
+# AP006222.2     ENSG00000228463        3  False                  3     0.001111              99.888889           3.0
+# RP11-206L10.2  ENSG00000228327        5  False                  5     0.001852              99.814815           5.0
+# RP11-206L10.9  ENSG00000237491        3  False                  3     0.001111              99.888889           3.0
+# LINC00115      ENSG00000225880       18  False                 18     0.006667              99.333333          18.0
+# ...                        ...      ...    ...                ...          ...                    ...           ...
+# AC145212.1     ENSG00000215750       16  False                 16     0.006667              99.407407          18.0
+# AL592183.1     ENSG00000220023      323  False                323     0.134815              88.037037         364.0
+# AL354822.1     ENSG00000215615        8  False                  8     0.002963              99.703704           8.0
+# PNRC2-1        ENSG00000215700      110  False                110     0.042963              95.925926         116.0
+# SRSF10-1       ENSG00000215699       69  False                 69     0.025926              97.444444          70.0
+
+adata_pbmc.obs
+#                   n_genes  n_genes_by_counts  total_counts  total_counts_mt  pct_counts_mt
+# AAACATACAACCAC-1      781                779        2419.0             73.0       3.017776
+# AAACATTGAGCTAC-1     1352               1352        4903.0            186.0       3.793596
+# AAACATTGATCAGC-1     1131               1129        3147.0             28.0       0.889736
+# AAACCGTGCTTCCG-1      960                960        2639.0             46.0       1.743085
+# AAACCGTGTATGCG-1      522                521         980.0             12.0       1.224490
+# ...                   ...                ...           ...              ...            ...
+# TTTCGAACTCTCAT-1     1155               1153        3459.0             73.0       2.110436
+# TTTCTACTGAGGCA-1     1227               1224        3443.0             32.0       0.929422
+# TTTCTACTTCCTCG-1      622                622        1684.0             37.0       2.197150
+# TTTGCATGAGAGGC-1      454                452        1022.0             21.0       2.054795
+# TTTGCATGCCTCAC-1      724                723        1984.0             16.0       0.806452
+
+# Use the violin plot/scatter plot to visualize the following quality control measures:
+# (1) the number of genes expressed with at least 1 count per barcode; 
+# (2) the total counts per barcode; 
+# (3) the percentage of counts in mitochondrial genes among total counts per barcode
+sc.pl.violin(adata_pbmc, ['n_genes_by_counts', 'total_counts', 'pct_counts_mt'],
+             jitter=0.5, multi_panel=True)
+
+
+sc.pl.scatter(adata_pbmc, x='total_counts', y='pct_counts_mt') # (2) vs (3)
+sc.pl.scatter(adata_pbmc, x='total_counts', y='n_genes_by_counts') # (2) vs (1)
+
+# Filter out cells that express too many mitochondrial genes or too high total counts/numbers of genes expressed with at least 1 count
+adata_pbmc = adata_pbmc[adata_pbmc.obs.pct_counts_mt < 5, :]
+adata_pbmc = adata_pbmc[adata_pbmc.obs.n_genes_by_counts < 2500, :]
+
+# Normalize the total count to 10000 reads per barcode
+sc.pp.normalize_total(adata_pbmc, target_sum=1e4)
+adata_pbmc.obs["total_counts"].describe()
+# adata_pbmc.obs.describe()
+adata_pbmc[:,:].to_df()
+#                   AL627309.1  AP006222.2  RP11-206L10.2  RP11-206L10.9  ...  AL592183.1  AL354822.1  PNRC2-1  SRSF10-1
+# AAACATACAACCAC-1         0.0         0.0            0.0            0.0  ...    0.000000    0.000000      0.0       0.0
+# AAACATTGAGCTAC-1         0.0         0.0            0.0            0.0  ...    2.039568    0.000000      0.0       0.0
+# AAACATTGATCAGC-1         0.0         0.0            0.0            0.0  ...    0.000000    0.000000      0.0       0.0
+# AAACCGTGCTTCCG-1         0.0         0.0            0.0            0.0  ...    0.000000    0.000000      0.0       0.0
+# AAACCGTGTATGCG-1         0.0         0.0            0.0            0.0  ...    0.000000    0.000000      0.0       0.0
+# ...                      ...         ...            ...            ...  ...         ...         ...      ...       ...
+# TTTCGAACTCTCAT-1         0.0         0.0            0.0            0.0  ...    0.000000    0.000000      0.0       0.0
+# TTTCTACTGAGGCA-1         0.0         0.0            0.0            0.0  ...    0.000000    0.000000      0.0       0.0
+# TTTCTACTTCCTCG-1         0.0         0.0            0.0            0.0  ...    0.000000    0.000000      0.0       0.0
+# TTTGCATGAGAGGC-1         0.0         0.0            0.0            0.0  ...    0.000000    9.784736      0.0       0.0
+# TTTGCATGCCTCAC-1         0.0         0.0            0.0            0.0  ...    0.000000    0.000000      0.0       0.0
+
+# Do the natural logarithm (log(x+1)) on the count in the barcode x gene count matrix
+sc.pp.log1p(adata_pbmc)
+adata_pbmc[:,:].to_df()
+#                   AL627309.1  AP006222.2  RP11-206L10.2  RP11-206L10.9  ...  AL592183.1  AL354822.1  PNRC2-1  SRSF10-1
+# AAACATACAACCAC-1         0.0         0.0            0.0            0.0  ...    0.000000    0.000000      0.0       0.0
+# AAACATTGAGCTAC-1         0.0         0.0            0.0            0.0  ...    1.111715    0.000000      0.0       0.0
+# AAACATTGATCAGC-1         0.0         0.0            0.0            0.0  ...    0.000000    0.000000      0.0       0.0
+# AAACCGTGCTTCCG-1         0.0         0.0            0.0            0.0  ...    0.000000    0.000000      0.0       0.0
+# AAACCGTGTATGCG-1         0.0         0.0            0.0            0.0  ...    0.000000    0.000000      0.0       0.0
+# ...                      ...         ...            ...            ...  ...         ...         ...      ...       ...
+# TTTCGAACTCTCAT-1         0.0         0.0            0.0            0.0  ...    0.000000    0.000000      0.0       0.0
+# TTTCTACTGAGGCA-1         0.0         0.0            0.0            0.0  ...    0.000000    0.000000      0.0       0.0
+# TTTCTACTTCCTCG-1         0.0         0.0            0.0            0.0  ...    0.000000    0.000000      0.0       0.0
+# TTTGCATGAGAGGC-1         0.0         0.0            0.0            0.0  ...    0.000000    2.378132      0.0       0.0
+# TTTGCATGCCTCAC-1         0.0         0.0            0.0            0.0  ...    0.000000    0.000000      0.0       0.0
