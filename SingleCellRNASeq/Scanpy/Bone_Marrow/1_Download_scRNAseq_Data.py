928bc4176daec302b12ff8bab718769545f5e3dd
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-06 11:31:55 -0400

    Update 1_Download_scRNAseq_Data.py

--- SingleCellRNASeq/Scanpy/Bone_Marrow/1_Download_scRNAseq_Data.py
+++ SingleCellRNASeq/Scanpy/Bone_Marrow/1_Download_scRNAseq_Data.py
@@ -6,5 +6,14 @@ from matplotlib import rcParams
 
 sc.settings.verbosity = 3  # verbosity: hints (3)
 sc.logging.print_versions()
-file_path = '../result/bone_marrow.h5ad'
+file_path = '../result/'
 sc.settings.set_figure_params(dpi=300, frameon=False, figsize=(4, 4), facecolor='white') 
+
+adata = sc.datasets.paul15()
+adata
+# AnnData object with n_obs × n_vars = 2730 × 3451
+#     obs: 'paul15_clusters'
+#     uns: 'iroot'
+
+adata.X = adata.X.astype('float64') # Data type conversion from float32 to float64
+adata.write(file_path + 'bone_marrow_raw.h5ad')
