117f4b84756726532f693fa4704667c4d3c238ef
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-06 10:56:08 -0400

    Update 1_Download_scRNAseq_Data.py

--- SingleCellRNASeq/Scanpy/Bone_Marrow/1_Download_scRNAseq_Data.py
+++ SingleCellRNASeq/Scanpy/Bone_Marrow/1_Download_scRNAseq_Data.py
@@ -3,3 +3,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as pl
 from matplotlib import rcParams
+
+sc.settings.verbosity = 3  # verbosity: hints (3)
+sc.logging.print_versions()
+file_path = '../result/bone_marrow.h5ad'
+sc.settings.set_figure_params(dpi=300, frameon=False, figsize=(4, 4), facecolor='white') 
