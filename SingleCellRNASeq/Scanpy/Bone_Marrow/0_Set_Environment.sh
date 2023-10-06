dfabf5c3624bf185bf1e01900c435fb5b5f1c682
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-06 11:21:06 -0400

    Update 0_Set_Environment.sh

--- SingleCellRNASeq/Scanpy/Bone_Marrow/0_Set_Environment.sh
+++ SingleCellRNASeq/Scanpy/Bone_Marrow/0_Set_Environment.sh
@@ -2,6 +2,8 @@ ls /Users/your_name/.pyenv/versions/
 # 3.11.6
 pyenv install 3.11.6 
 pyenv global 3.11.6 
+python --version
+# Python 3.11.6
 # Notice that Scanpy cannot be installed on Python version 3.12.0; only versions >=3.8,<3.12 are supported;
-# So we switch to a Pyenv-installed Python 3.11.6 globally
+# So we switch to a Pyenv-installed Python 3.11.6 globally.
 pip install scanpy
