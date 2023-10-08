5eab160af7aeb079d659c45752b92320478a146a
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-08 09:44:07 -0400

    Update 2_Train_Variational_Autoencoder.py

--- SingleCellRNASeq/Perturbation/scGen/CD4TCell/2_Train_Variational_Autoencoder.py
+++ SingleCellRNASeq/Perturbation/scGen/CD4TCell/2_Train_Variational_Autoencoder.py
@@ -1,18 +1,28 @@
+# pip install anndata==0.9.2
+# pip install git+https://github.com/theislab/scgen.git
+# pip install pertpy
+# pip install scanpy
+# pip install numba
+# pip install llvmlite
+
 import os
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 import scanpy as sc
 import pertpy as pt
 import scgen
+file_path = 'gdrive/MyDrive/Perturbation/'
 
-# Create and save the model 
-# Recap: adata_train = adata[~((adata.obs["cell_type"] == "CD4 T cells") & (adata.obs["condition"] == "stimulated"))].copy()
+# Create the model 
+# Recap: 
+# adata_train = adata[~((adata.obs["cell_type"] == "CD4 T cells") & (adata.obs["condition"] == "stimulated"))].copy()
+adata_train.shape # (18995, 15706)
+scgen.SCGEN.setup_anndata(adata_train, batch_key="condition", labels_key="cell_type")
 model = scgen.SCGEN(adata_train, n_hidden=800, n_latent=100, n_layers=2)
 """
 n_hidden: number of nodes per hidden layer
 n_latent: dimensionality of the latent space
 n_layers: number of hidden layers used for encoder and decoder NNs
 """
-model.save("../model/perturbation.pt", overwrite=True)
 
 # Train the model
 model.train(max_epochs=100, batch_size=32, early_stopping=True, early_stopping_patience=25)
@@ -22,9 +32,18 @@ batch_size: number of samples (individual cells) the model sees to update its pa
 early_stopping: which enables the model to stop the training if its results are not improved after `early_stopping_patience` training epochs; the early stopping mechanism prevents potential overfitting of the training data, which can lead to poor generalization to unseen cell populations
 """
 
+type(model) # scgen._scgen.SCGEN
+
+# Save and reload the model
+model.save(file_path + 'single_cell_data/perturbation_model.pt', overwrite=True, save_anndata=True)
+model = scgen.SCGEN.load(file_path + 'single_cell_data/perturbation_model.pt')
+
+# Show the learned latent space
+model.get_latent_representation()
+model.get_latent_representation().shape # (18995, 100)
+
 # Visualize the representation of data learned by the model using UMAP
 adata_train.obsm["scgen"] = model.get_latent_representation() # get_latent_representation() returns a 100-dimensional vector for each cell
-adata_train
 adata_train.obsm["scgen"]
 # array([[ -7.4319363 ,   6.0347595 ,   4.360623  , ...,  -4.1190267 ,
 #           0.4719472 ,   5.999382  ],
