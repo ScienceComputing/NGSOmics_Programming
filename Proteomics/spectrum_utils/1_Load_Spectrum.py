172c88a6ee7e4483412f9fac74a518dbb120fbd0
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-15 18:20:49 -0400

    Update 1_Load_Spectrum.py

--- Proteomics/spectrum_utils/1_Load_Spectrum.py
+++ Proteomics/spectrum_utils/1_Load_Spectrum.py
@@ -1,3 +1,42 @@
+# pip install spectrum_utils
+
 import spectrum_utils.plot as sup
 import spectrum_utils.spectrum as sus
 import matplotlib.pyplot as plt
+
+import matplotlib.pyplot as plt
+import spectrum_utils.plot as sup
+import spectrum_utils.spectrum as sus
+
+# Read into a spectrum from public data repositories by its Universal Spectrum Identifier.
+# https://www.psidev.info/usi 
+usi_id = "mzspec:PXD000966:CPTAC_CompRef_00_iTRAQ_05_2Feb12_Cougar_11-10-09.mzML:scan:12298:[iTRAQ4plex]-LHFFM[Oxidation]PGFAPLTSR/3"
+peptide = "WNQLQAFWGTGK"
+spectrum = sus.MsmsSpectrum.from_usi(usi_id)
+
+# Perform the quality control
+fragment_tol_mass, fragment_tol_mode = 10, "ppm"
+spectrum = (
+    # Focus the mass range to 100 – 1400 m/z to filter out irrelevant peaks
+    spectrum.set_mz_range(min_mz=100, max_mz=1400)
+    # Remove the precursor peak
+    .remove_precursor_peak(fragment_tol_mass, fragment_tol_mode)
+    # Remove noise peaks of low intensity
+    # Keep peaks that are at at least 5% of the base peak intensity; restrict the total number of peaks to the 50 most intense peaks
+    .filter_intensity(min_intensity=0.05, max_num_peaks=50)
+    # Scale the peak intensities by their square root, to de-emphasize overly intense peaks
+    .scale_intensity("root")
+    # Annotate peaks corresponding to a, b, and y peptide fragments
+    .annotate_proforma(
+        peptide, fragment_tol_mass, fragment_tol_mode, ion_types="aby"
+    )
+)
+
+# Visualize the spectrum with the annotated peaks 
+fig, ax = plt.subplots(figsize=(12, 6))
+sup.spectrum(spectrum, grid=False, ax=ax)
+ax.spines["right"].set_visible(False)
+ax.spines["top"].set_visible(False)
+plt.show()
+# plt.savefig("trial.png", bbox_inches="tight", dpi=300, transparent=True)
+# plt.close()
