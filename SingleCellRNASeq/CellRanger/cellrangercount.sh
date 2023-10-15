8c330f877042c7231d53c34e49732ced5d9fba9f
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-15 18:04:25 -0400

    Create cellrangercount.sh

---
+++ SingleCellRNASeq/CellRanger/cellrangercount.sh
@@ -0,0 +1,19 @@
+# Set up the environment
+work_dir = "/Users/your_name/SingleCell"
+mkdir data result
+cd $work_dir/result/
+
+# Download the FASTQ data and prebuilt human reference transcriptome
+wget https://cf.10xgenomics.com/samples/cell-exp/3.0.0/pbmc_1k_v3/pbmc_1k_v3_fastqs.tar
+tar -xvf pbmc_1k_v3_fastqs.tar
+wget https://cf.10xgenomics.com/supp/cell-exp/refdata-gex-GRCh38-2020-A.tar.gz
+tar -zxvf refdata-gex-GRCh38-2020-A.tar.gz
+
+# Align reads and generate Feature Barcode matrices
+cellranger count --id=pbmc_1k_run_count \
+   --fastqs=../data/pbmc_1k_v3_fastqs \
+   --sample=pbmc_1k_v3 \
+   --transcriptome=../data/refdata-gex-GRCh38-2020-A
+
+# List the output
+ls -1 outs # Display one entry per line. 
