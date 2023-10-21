631e5a452a3edd7b1ffa640e029a6d47cf18fea3
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-21 17:27:42 -0400

    Update bcl_to_fastq.sh

--- FastQC/bcl_to_fastq.sh
+++ FastQC/bcl_to_fastq.sh
@@ -9,12 +9,14 @@ bcl2fastq --use-bases-mask=Y26,I8,Y98 \
             --ignore-missing-filter \
             --ignore-missing-bcls \
             -r 6 -w 6 \
-            -R ../data \
+            -R ../flow_cell/data \
             --output-dir=../fastq \
             --interop-dir=../op \
             --sample-sheet=../sample_sheet.csv
 
-
+# --use-bases-mask: specify the number of cycles to use in each read and how to use them
+# y = read, i = index, n = ignore; the default values are assumed from the RunInfo.xml file 
+# Usage for all cycles: --use-bases-mask=Y*,I*,Y*
 # -R ${FLOWCELL_DIR}: specify the directory that contains a flow cell's Data folder
 # --output-dir=${OUTPUT_DIR}: specify the directory that we want to output the converted FASTQs to
 # --interop-dir=${INTEROP_DIR}: specify the directory where InterOp files are located. InterOp files contain various run metrics.
