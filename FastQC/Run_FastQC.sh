31cfee8b8199b588824a4513d77f16fc7fdc8d9c
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-21 17:19:27 -0400

    Update Run_FastQC.sh

--- FastQC/Run_FastQC.sh
+++ FastQC/Run_FastQC.sh
@@ -1,12 +1,15 @@
 # https://www.bioinformatics.babraham.ac.uk/projects/fastqc/INSTALL.txt
 
-# Perform quality control analysis on particular or all FASTQ files stored in compressed (gzipped) format. 
+# Locate to the desired directory
 cd scRNA/data
+
 # View the first 6 entries
 zcat HumanBrain_S1_L001_R1_001.fastq.gz | head -6 
+
 # View the number of lines in the FASTQ file
 zcat HumanBrain_S1_L001_R1_001.fastq.gz | wc -l
 
+# Perform quality control analysis on particular or all FASTQ files stored in compressed (gzipped) format.
 zcat HumanBrain_S1_L001_R1_001.fastq.gz | fastqc stdin --outdir=../report/
 
 zcat *fastq.gz | fastqc stdin --outdir=../report/
