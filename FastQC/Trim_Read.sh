430ce1d32d8919bf4cc73d09f122abbdbce0f0cc
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-21 18:14:37 -0400

    Update Trim_Read.sh

--- FastQC/Trim_Read.sh
+++ FastQC/Trim_Read.sh
@@ -1,5 +1,6 @@
 # Use FastQC to check read quality again after trimming; FastQC should show that our reads have nice quality on the 'Per Base Sequence Quality' and 'Adaptor Content' plots 
+# https://github.com/FelixKrueger/TrimGalore/blob/master/Docs/Trim_Galore_User_Guide.md
 
 cd report
 mkdir trimmed_results
-trim_galore --nextera -o trimmed_results ../data/file_1.fastq ../data/file_2.fastq
+trim_galore --illumina --paired -o trimmed_results ../data/file_trimmed_1.fastq ../data/file_trimmed_2.fastq
