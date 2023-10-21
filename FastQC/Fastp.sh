4fdb4049bec4985462b5d6a5f8d48a15e4d35300
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-21 18:03:14 -0400

    Create Fastp.sh

---
+++ FastQC/Fastp.sh
@@ -0,0 +1,1 @@
+# Fastp is an 'all-in-one' preprocessing tool designed for use with fastq files that seamlessly incorporates various quality profiling aspects for both pre- and post-filtering data. These include quality curves, base composition, KMER analysis, Q20/Q30 evaluation, GC ratio assessment, duplication detection, adapter content, and more.
