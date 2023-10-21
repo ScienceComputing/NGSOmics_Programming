af7260f7cd44e029c16b703effb1f83eb00d85bc
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-21 17:15:17 -0400

    Update bcl_to_fastq.sh

--- FastQC/bcl_to_fastq.sh
+++ FastQC/bcl_to_fastq.sh
@@ -9,7 +9,13 @@ bcl2fastq --use-bases-mask=Y26,I8,Y98 \
             --ignore-missing-filter \
             --ignore-missing-bcls \
             -r 6 -w 6 \
-            -R ${FLOWCELL_DIR} \
-            --output-dir=${OUTPUT_DIR} \
-            --interop-dir=${INTEROP_DIR} \
-            --sample-sheet=${SAMPLE_SHEET_PATH}
+            -R ../data \
+            --output-dir=../fastq \
+            --interop-dir=../op \
+            --sample-sheet=../sample_sheet.csv
+
+
+# -R ${FLOWCELL_DIR}: specify the directory that contains a flow cell's Data folder
+# --output-dir=${OUTPUT_DIR}: specify the directory that we want to output the converted FASTQs to
+# --interop-dir=${INTEROP_DIR}: specify the directory where InterOp files are located. InterOp files contain various run metrics.
+# --sample-sheet=${SAMPLE_SHEET_PATH}: specify the path to the sample sheet CSV we create
