769b2818e92c3ac40666b747a2f6dedbbe05f204
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-27 09:43:24 -0700

    Update 2_Stage_Unstage_File.sh

--- Git/Workflow/2_Stage_Unstage_File.sh
+++ Git/Workflow/2_Stage_Unstage_File.sh
@@ -3,3 +3,15 @@ git add file.py
 
 # Add all files in the current directory to the staging area
 git add .
+
+# Unstage a single file
+git reset HEAD file.py
+# Modify this file
+vim file.py
+# Again add this file to the staging area
+git add file.py
+# Make the commit
+git commit -m "XXX"
+
+# Unstage all files in the current directory to the staging area
+git reset HEAD
