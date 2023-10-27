60dc8fc88a10c57e4c9ea8c8a4b241ff19e4162f
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-27 09:04:43 -0700

    Update 6_View_Commit_Info.sh

--- Git/Workflow/6_View_Commit_Info.sh
+++ Git/Workflow/6_View_Commit_Info.sh
@@ -1,4 +1,4 @@
 # Display all commits made to the repo in chronological order, starting with the oldest 
 # Show the commit hash, author, date, and commit message
-# git log command is very useful for tracking changes at a high level before diving deeper into more specific version control tasks
+# git log command is very useful for tracking hash and changes at a high level before diving deeper into more specific version control tasks
 git log
