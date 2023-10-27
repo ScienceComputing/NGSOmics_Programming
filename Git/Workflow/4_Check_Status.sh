f5db325e3eca593bbb19a361a8505adf3f68c5d9
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-26 19:54:47 -0700

    Update 4_Check_Status.sh

--- Git/Workflow/4_Check_Status.sh
+++ Git/Workflow/4_Check_Status.sh
@@ -1,2 +1,2 @@
-# Use the git status command to check which files are in the staging area, and which files have changes that aren't in the staging area yet
+# Use the git status command to check which files are in the staging area, and which files have changes that aren't in the staging area (git add) yet
 git status
