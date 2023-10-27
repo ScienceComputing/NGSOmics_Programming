1facd44f75fdee7fcf36416add6bdd2bd1113642
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-27 09:26:18 -0700

    Update 7_View_Particular_Commit.sh

--- Git/Workflow/7_View_Particular_Commit.sh
+++ Git/Workflow/7_View_Particular_Commit.sh
@@ -1,2 +1,6 @@
 # It's common to refer to the log (git log) and then perform a follow-up command (git show hash) to find a specific piece of information
+# View changes made in a particular commit
 git show first_6_8_characters_of_hash
+
+# View changes made to files in a second-to-most recent commit
+git show HEAD~1
