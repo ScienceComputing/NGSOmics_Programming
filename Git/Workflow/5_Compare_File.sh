9606b39d9317d139cacbb334ab35c272fb81e72d
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-27 09:22:01 -0700

    Update 5_Compare_File.sh

--- Git/Workflow/5_Compare_File.sh
+++ Git/Workflow/5_Compare_File.sh
@@ -6,3 +6,11 @@ git diff -r HEAD file_name
 
 # Compare multiple staged files with the last committed versions
 git diff -r HEAD
+
+# Display the differences between the current state of the specified file and its state in the previous commit (the commit just before the current one)
+# Helpful for reviewing changes made to the file in the most recent commit
+git diff -r HEAD~1 file_name
+
+# Display the differences between the current state of the specified file and its state two commits ago
+# Useful for seeing how a particular file has changed over the course of two commits
+git diff -r HEAD~2 file_name
