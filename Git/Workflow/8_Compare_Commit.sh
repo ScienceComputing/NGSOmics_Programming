224420364afcf36c650bfa969fe8998160b86a4c
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-27 09:29:22 -0700

    Update 8_Compare_Commit.sh

--- Git/Workflow/8_Compare_Commit.sh
+++ Git/Workflow/8_Compare_Commit.sh
@@ -1,3 +1,6 @@
 # View the differences between two commits
 git diff hash_1 hash_2
 # hash_1: first 6 - 8 characters of a hash
+
+# View the differences betwen the third-most recent commit and second-most recent commit
+git diff HEAD~2 HEAD~1
