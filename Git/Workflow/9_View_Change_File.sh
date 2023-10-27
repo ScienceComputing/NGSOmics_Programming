f3b3fb19c635d3202eaafe28ccf15efece57eb4f
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-27 09:32:16 -0700

    Update 9_View_Change_File.sh

--- Git/Workflow/9_View_Change_File.sh
+++ Git/Workflow/9_View_Change_File.sh
@@ -1,2 +1,3 @@
 # Track who made the last change to each line of a file, and when the change took place
+# if we are working as part of a large team where multiple people are editing a file, then git annotate is a quick way to see who added a specific line of content and when
 git annotate file_name
