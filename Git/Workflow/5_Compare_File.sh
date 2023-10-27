a22262e70467d79a8cf686852e792ce9924edec5
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-27 09:17:02 -0700

    Update 5_Compare_File.sh

--- Git/Workflow/5_Compare_File.sh
+++ Git/Workflow/5_Compare_File.sh
@@ -1,7 +1,7 @@
 # Compare an unstaged file with the last committed version
 git diff file_name
 
-# Compare a staged file with the last committed version
+# Compare a staged (git add) file with the last committed version
 git diff -r HEAD file_name
 
 # Compare multiple staged files with the last committed versions
