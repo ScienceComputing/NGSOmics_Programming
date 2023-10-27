204f1520057b8fde0f67fe268f6d0da10ff52f60
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-26 19:57:18 -0700

    Update 2_Save_File.sh

--- Git/Workflow/2_Save_File.sh
+++ Git/Workflow/2_Save_File.sh
@@ -1,5 +1,5 @@
-# Add a single file
+# Add a single file to the staging area
 git add file.py
 
-# Add all files in the current directory
+# Add all files in the current directory to the staging area
 git add .
