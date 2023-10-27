25b4a5403e347cdfce9c255c798dccf5fedb356a
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-27 09:07:09 -0700

    Update 7_View_Particular_Commit.sh

--- Git/Workflow/7_View_Particular_Commit.sh
+++ Git/Workflow/7_View_Particular_Commit.sh
@@ -1,2 +1,2 @@
-# It's common to refer to the git log and then perform a follow-up command (git show hash) to find a specific piece of information
+# It's common to refer to the log (git log) and then perform a follow-up command (git show hash) to find a specific piece of information
 git show first_6_8_characters_of_hash
