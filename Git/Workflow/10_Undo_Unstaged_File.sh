8a5843ea78389beb6a35ff1d7d8e78018728424d
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-27 19:38:08 -0700

    Create 10_Undo_Unstaged_File.sh

---
+++ Git/Workflow/10_Undo_Unstaged_File.sh
@@ -0,0 +1,5 @@
+# Undo changes to an unstaged file; this will wipe out all changes made to the unstaged file forever
+git checkout -- file_name
+
+# Undo changes to all unstaged files in the current directory and subdirectories
+git checkout .
