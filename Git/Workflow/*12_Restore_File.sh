bd2ba209c88479eabb5321bc6982b0e8119f903c
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-27 21:00:19 -0700

    Update *12_Restore_File.sh

--- Git/Workflow/*12_Restore_File.sh
+++ Git/Workflow/*12_Restore_File.sh
@@ -2,8 +2,9 @@
 git log --since='Mar 3 2023' # Find the hash
 git checkout first_6_8_hash file_name
 
-# Later ...
+# Put the restored version into the staging area
 git add file_name
+# Commit the restored file with a log message 
 git commit -m "Restoring version from commit first_6_8_hash"
 
 # Revert to a version from the second-to-most recent commit
