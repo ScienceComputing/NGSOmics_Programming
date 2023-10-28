8ce2714ea90ec5e578472290699ad52adba563d6
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-27 20:50:40 -0700

    Update 11_Restore_Repo.sh

--- Git/Workflow/11_Restore_Repo.sh
+++ Git/Workflow/11_Restore_Repo.sh
@@ -1,4 +1,11 @@
+# Restore a repo to the most recent commit
 git read HEAD
 git checkout .
 git add .
 git commit -m 'Restore the repo to previous commit'
+
+# Restore a repo to a specific commit
+git checkout first_6_8_hash
+
+# Restore a repo to the second-to-most recent commit
+git checkout HEAD~1
