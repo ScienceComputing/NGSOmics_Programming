dcc1a26cacb5c0beb307110be0df8904fd2e1e6e
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-27 20:59:49 -0700

    Update and rename 12_Restore_File.sh to *12_Restore_File.sh

--- Git/Workflow/*12_Restore_File.sh
+++ Git/Workflow/*12_Restore_File.sh
@@ -2,5 +2,11 @@
 git log --since='Mar 3 2023' # Find the hash
 git checkout first_6_8_hash file_name
 
+# Later ...
+git add file_name
+git commit -m "Restoring version from commit first_6_8_hash"
+
 # Revert to a version from the second-to-most recent commit
 git checkout HEAD~1 file_name
+
+
