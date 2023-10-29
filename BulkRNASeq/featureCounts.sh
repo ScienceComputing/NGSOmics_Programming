4b983f306c90dd7d810ed475784921554d53da8a
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-29 13:25:40 -0400

    Update featureCounts.sh

--- BulkRNASeq/featureCounts.sh
+++ BulkRNASeq/featureCounts.sh
@@ -1,6 +1,10 @@
+# Summarize paired-end reads and count fragments 
 featureCounts -T 20 \
+-p --countReadPairs
 -t exon \
 -g gene_id \
 -a annotation.gtf \
 -o human_featureCounts_output.txt \
 Human_Infectious_Disease_Aligned.sortedByCoord.out.bam
+
+# T: use 20 threads
