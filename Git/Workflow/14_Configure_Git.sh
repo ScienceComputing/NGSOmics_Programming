866d3209633624c15c24a8a7c1335ab2f39ebd8c
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-11-04 14:52:57 -0400

    Create 14_Configure_Git.sh

---
+++ Git/Workflow/14_Configure_Git.sh
@@ -0,0 +1,13 @@
+# Access a list of customizable settings
+git config --list
+
+
+
+# Settings for one specific project
+git config --local
+
+# Settings for all projects
+git config --global
+
+# Settings for every user on this computer
+git config --system
