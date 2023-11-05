71d16ba29c8a66e656116fa59aba4876cd163d65
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-11-05 12:10:47 -0500

    Create Preprocess.sh

---
+++ ChIPSeq/Preprocess.sh
@@ -0,0 +1,8 @@
+# Organize the directories
+mkdir -p raw_fastq qc_reports trimmed_fastq aligned_bam sorted_bam peak_calls peak_annotations tag_directories profiles motifs
+
+# QC with FastQC
+fastqc raw_fastq/*.fastq.gz -o qc_reports/
+
+# Trim the sequences
+cutadapt -a GATCGGAAGAGCACACGTCTGAACTCCAGTCACCGA -o trimmed_fastq/sample_trim.fastq.gz raw_fastq/sample.fastq.gz
