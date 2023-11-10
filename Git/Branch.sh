988f12053fde2336134913afe7b14036818850b5
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-11-10 08:08:53 -0500

    Update Branch.sh

--- Git/Branch.sh
+++ Git/Branch.sh
@@ -1,1 +1,9 @@
-# Why use braches: systematically track multiple versions of files
+# Why use braches?
+# Systematically track multiple versions of files
+# Minimize the risk of conflicting versions
+# Avoid endless subdirectories
+# Allow multiple users to work simultaneously
+
+# Display branches
+git branch
+# *: current branch
