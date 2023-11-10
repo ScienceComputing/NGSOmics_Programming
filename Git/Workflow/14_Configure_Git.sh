fcfb1ceca5643b8dbc87fd646eb7c0c96fcd3ac3
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-11-09 21:39:39 -0500

    Update 14_Configure_Git.sh

--- Git/Workflow/14_Configure_Git.sh
+++ Git/Workflow/14_Configure_Git.sh
@@ -8,6 +8,13 @@ git config --local
 
 # Settings for all projects
 git config --global
+git config --global user.email XXX@gmail.com
+git config --global user.name 'YYY UUU'
+
+# !Create an alias for committing files by executing ci
+config --global alias.ci 'commit -m'
+# Commit files 
+git ci F100.fastq.gz 'Upload the FASTQ file'
 
 # Settings for every user on this computer
 git config --system
