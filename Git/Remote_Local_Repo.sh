2cac9ca8dc07ea748d93e1624460243915dc07c4
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-11-11 09:09:09 -0500

    Update Remote_Local_Repo.sh

--- Git/Remote_Local_Repo.sh
+++ Git/Remote_Local_Repo.sh
@@ -1,4 +1,5 @@
 # To compare the files in a remote repo against the contents of a local repo
+# Gather contents from the remote origin repo into the local main branch
 git fetch origin main
 # origin: name of the remote to fetch from
 # main: branch of the local repo to fetch into
