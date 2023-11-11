eca9c316090ad14e3ff28453c4668775306228d9
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-11-11 09:00:15 -0500

    Update 7_View_Particular_Commit.sh

--- Git/Workflow/7_View_Particular_Commit.sh
+++ Git/Workflow/7_View_Particular_Commit.sh
@@ -2,5 +2,5 @@
 # View changes made in a particular commit
 git show first_6_8_characters_of_hash
 
-# View changes made to files in a second-to-most recent commit
+# View changes made to files in a *second-to-most* recent commit
 git show HEAD~1
