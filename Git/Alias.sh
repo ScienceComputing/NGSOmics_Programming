db79c22ae28d5444d1570d71cf6803aa21fa2b17
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-11-11 09:41:37 -0500

    Create Alias.sh

---
+++ Git/Alias.sh
@@ -0,0 +1,8 @@
+# Create an alias named ga for the `git add` command
+git config --global alias.ga add
+ga scRNAseq.py
+
+# Create an alias named gc for the `git commit` command
+git config --global alias.gc commit
+gc -m scRNAseq.py 'Add a scRNAseq python file'
+
