60b7dccae6dfcc96ec3840dfbdfdc5a652761bf8
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-11-11 09:31:35 -0500

    Update Local_Remote_Repo.sh

--- Git/Local_Remote_Repo.sh
+++ Git/Local_Remote_Repo.sh
@@ -4,3 +4,8 @@
 # Push into remote from local branch
 git push remote_name local_branch_name
 git push origin main
+
+# BUT: if there are different commits on the local and remote repos
+git pull origin main
+git pull --no-edit origin main # Avoid the message
+git push origin main
