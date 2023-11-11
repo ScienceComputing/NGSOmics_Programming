686f0a298ce60ee05bbe795e2db565028d83693f
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-11-11 08:29:01 -0500

    Create Create_Repo.sh

---
+++ Git/Create_Repo.sh
@@ -0,0 +1,12 @@
+# ! Do not create a Git repo inside another Git repo
+
+git init new_repo_name
+cd new_repo_name
+
+# Check if the repo has been initialized correctly
+git status
+
+# Covert an exsiting project into a repo
+# Run the following command from the project directory
+git init
+git status # Check if the repo has been initialized correctly
