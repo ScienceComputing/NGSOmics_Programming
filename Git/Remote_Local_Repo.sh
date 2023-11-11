09255e3d6eb82f5bfb7edb4fd1e4c7cad3c9b6cd
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-11-11 08:59:51 -0500

    Create Remote_Local_Repo.sh

---
+++ Git/Remote_Local_Repo.sh
@@ -0,0 +1,4 @@
+# To compare the files in a remote repo against the contents of a local repo
+git fetch origin main
+# origin: name of the remote to fetch from
+# main: branch of the local repo to fetch into
