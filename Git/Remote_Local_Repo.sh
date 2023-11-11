6fd1d6e20f4fc6eb9b943501fb2940b8e082e8fa
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-11-11 09:05:07 -0500

    Update Remote_Local_Repo.sh

--- Git/Remote_Local_Repo.sh
+++ Git/Remote_Local_Repo.sh
@@ -2,3 +2,14 @@
 git fetch origin main
 # origin: name of the remote to fetch from
 # main: branch of the local repo to fetch into
+
+git fetch origin specific_local_branch_name
+
+# After fetching, we have the contents of the remote in our local repo
+# We need to synchronize contents between the 2 repos
+git merge origin main
+
+# The remote repository often contains more up-to-date information than local repositories, making it a key source of truth. 
+# Consequently, the process of fetching content from the remote and synchronizing it with the local repo is a common workflow.
+# ! Fetch and merge together
+git pull origin main
