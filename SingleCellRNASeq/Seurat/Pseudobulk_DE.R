bec146b60f76c8422af4590f2c83eb5c3dfbdae2
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-11-13 09:10:22 -0500

    Update Pseudobulk_DE.R

--- SingleCellRNASeq/Seurat/Pseudobulk_DE.R
+++ SingleCellRNASeq/Seurat/Pseudobulk_DE.R
@@ -1,1 +1,3 @@
-# TD
+# Before conducting differential gene expression (DGE) analysis, it's essential to aggregate the expression values specific to each cell type within an individual. 
+# This aggregation can be achieved by using either a sum, mean, or random effect for each individual. 
+# This process, known as pseudobulk generation, is critical for addressing correlations that exist within a sample.
