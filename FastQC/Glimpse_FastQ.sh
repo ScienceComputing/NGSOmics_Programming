b825ce9fba2e3829c0e32463960b8b7880a776c3
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-01-06 17:50:39 -0500

    Update Glimpse_FastQ.sh

--- FastQC/Glimpse_FastQ.sh
+++ FastQC/Glimpse_FastQ.sh
@@ -3,7 +3,9 @@ gzip -cd SRR_number.fastq.gz | head -n 12
 
 # View the whole records of N bases in one/multiple FASTQ files
 grep -B 1 -A 2 N SRR_number.fq
+grep -B 1 -A 2 -n N SRR_number.fq # -n: show the line number
 grep -B 1 -A 2 N SRR*
+grep -B 1 -A 2 -n N SRR*
 
 # View the whole records of N bases in the compressed FASTQ file
 gzip -cd SRR_number.fastq.gz | grep -B 1 -A 2 N
