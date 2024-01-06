bed62da2a2e99301725e474f04704d3d1a7ec162
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-01-06 11:50:08 -0500

    Update and rename Glimpse_FASTQ.sh to Glimpse_FastQ.sh

--- FastQC/Glimpse_FastQ.sh
+++ FastQC/Glimpse_FastQ.sh
@@ -1,8 +1,9 @@
 # Print the first 3 sequecing reads
 gzip -cd SRR_number.fastq.gz | head -n 12
 
-# View N bases
+# View the whole records of N bases in one/multiple FASTQ files
 grep -B 1 -A 2 N SRR_number.fq
+grep -B 1 -A 2 N SRR*
 
 
 
