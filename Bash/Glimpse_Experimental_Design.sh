7ee59f11e397590f9d6b62d9a07f4bbc49a4ec4a
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-01-06 20:31:26 -0500

    Update Glimpse_Experimental_Design.sh

--- Bash/Glimpse_Experimental_Design.sh
+++ Bash/Glimpse_Experimental_Design.sh
@@ -1,5 +1,5 @@
 # Print the meta information excluding the cell type of treatment1
 grep -v treatment1 ~/scRNAseq/cell_metadata.txt 
 
-# How many cell types in this meta dataset?
+# How many cell types are present in this meta dataset?
 cut -f 2 ~/scRNAseq/cell_metadata.txt | sort -ufb | wc -l
