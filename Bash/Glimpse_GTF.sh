06b3f633246528cc1512f560dbbf539cef3998a2
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-01-06 20:22:42 -0500

    Update Glimpse_GTF.sh

--- Bash/Glimpse_GTF.sh
+++ Bash/Glimpse_GTF.sh
@@ -1,3 +1,10 @@
 # https://hgdownload.soe.ucsc.edu/downloads.html#human
 grep ENST00000473358 hg38.knownGene.gtf | head -n 6
 grep ENST00000473358 hg38.knownGene.gtf | wc -l # Count the number of transcript isoforms associated with the gene ENST00000473358
+
+# View the 1st and 9th columns of a GTF file
+cut -f 1,9 hg38.knownGene.gtf | head -n 6
+cut -f 1,9 hg38.knownGene.gtf | less
+
+# View the contents in the 9th column seperated by ';'
+cut -f 9 hg38.knownGene.gtf | cut -f 1-4 -d ';' | head -n 3
