e6f97b1425cf9e8e20891353af997f1e36a8174f
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-01-06 20:41:24 -0500

    Update Glimpse_GTF.sh

--- Bash/Glimpse_GTF.sh
+++ Bash/Glimpse_GTF.sh
@@ -8,3 +8,7 @@ cut -f 1,9 hg38.knownGene.gtf | less
 
 # View the contents in the 9th column seperated by ';'
 cut -f 9 hg38.knownGene.gtf | cut -f 1-4 -d ';' | head -n 3
+
+# Count the unique exons on chromosome 6
+awk '/exon/ && /chr6/' hg38.knownGene.gtf | cut -f 1,3 | sort -ubf | wc -l
+awk '/exon/ && /chr6/' hg38.knownGene.gtf | cut -f 1,3 | head -n 3 
