95c505511d4aa38dcf4c1ed08bd3976870e3308f
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-01-22 09:17:11 -0500

    Update Glimpse_GTF.sh

--- Bash/Glimpse_GTF.sh
+++ Bash/Glimpse_GTF.sh
@@ -1,6 +1,10 @@
+# References:
 # https://hgdownload.soe.ucsc.edu/downloads.html#human
+# https://www.gencodegenes.org/human/
+# http://mblab.wustl.edu/GTF22.html
 grep ENST00000473358 hg38.knownGene.gtf | head -n 6
 grep ENST00000473358 hg38.knownGene.gtf | wc -l # Count the number of transcript isoforms associated with the gene ENST00000473358
+grep HAVANA gencode.v45.primary_assembly.annotation.gtf | head -n 6
 
 # View the 1st and 9th columns of a GTF file
 cut -f 1,9 hg38.knownGene.gtf | head -n 6
