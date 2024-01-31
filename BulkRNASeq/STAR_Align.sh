5e8690dd6776d7363fe01ae3786358b8a429936b
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-01-31 08:26:37 -0500

    Update STAR_Align.sh

--- BulkRNASeq/STAR_Align.sh
+++ BulkRNASeq/STAR_Align.sh
@@ -21,18 +21,23 @@ STAR --runThreadN 20 \
 --sjdbGTFfile /Users/your_name/bulk_RNAseq/data/reference/homo_sapiens.GRCh38.92.gtf \
 --sjdbOverhang 99
 
-# genomeDir: provide the path to store the genome indices
-# genomeFastaFiles: provide the path to one or more reference genomes in FASTA format
-# sjdbGTFfile: provide the path to the annotations of the reference genome in GTF format
-# sjdbOverhang: this is the length of the genomic sequence around the annotated junction to be used in constructing the splice junctions database. Ideally, this length should equal to the max(ReadLength)-1, where ReadLength is the length of the reads. e.g., for Illumina 2x100b paired-end reads, the ideal value is 100-1=99. Often, 100 works quite well.
+# genomeDir: the path to store the genome indices
+# genomeFastaFiles: the path to one or more reference genomes in FASTA format
+# sjdbGTFfile: the path to the annotations of the reference genome in GTF format
+# sjdbOverhang: the length of the genomic sequence around the annotated junction to be used in constructing the splice junctions database. Ideally, this length should equal to the max(ReadLength)-1, where ReadLength is the length of the reads. e.g., for Illumina 2x100b paired-end reads, the ideal value is 100-1=99. Often, 100 works quite well.
 
 # Map reads to the reference genome
 STAR --genomeDir /Users/your_name/bulk_RNAseq/data/hg38_chr1_index \
 --runThreadN 20 \
---readFilesIn /Users/your_name/bulk_RNAseq/raw_data/s1read1.fq.gz,s2read1.fq.gz s1read2.fq.gz,s2read2.fq.gz \
+--runMode alignReads
+--readFilesIn /Users/your_name/bulk_RNAseq/data/fastq/s1read1.fq.gz,s2read1.fq.gz s1read2.fq.gz,s2read2.fq.gz \
+--readFilesCommand zcat
 --outFileNamePrefix /Users/your_name/bulk_RNAseq/results/STAR/Human_Infectious_Disease_ \
 --outSAMtype BAM SortedByCoordinate \
 --outSAMunmapped Within \
 --outSAMattributes Standard 
 
-# STAR has the capability to generate alignments in the binary BAM format directly, eliminating the need to convert SAM files to BAM, which can save our time. Additionally, STAR can sort BAM files by coordinates, a necessary step for numerous downstream applications.
+# use --readFilesCommand bunzip2 -c for bzip2 compressed files
+
+
+
