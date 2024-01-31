dd11138931135dbcce49d98336031c6b12f83c2b
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-01-31 08:47:28 -0500

    Create BWA_align.sh

---
+++ BulkRNASeq/BWA_align.sh
@@ -0,0 +1,2 @@
+# Reference: https://bio-bwa.sourceforge.net/bwa.shtml
+bwa mem ~/bulkRNAseq/data/reference_genome/yeast_index_bwa/sacCer3 ~/bulkRNAseq/data/fastq/ERRXXXXX.fastq.gz > ~/bulkRNAseq/result/alignment/ERRXXXXX.bwa.sam
