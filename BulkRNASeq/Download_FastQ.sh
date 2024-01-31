b77134fd956b6b166405ece4a354a2ff3d3409f4
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-01-30 20:22:04 -0500

    Update Download_FastQ.sh

--- BulkRNASeq/Download_FastQ.sh
+++ BulkRNASeq/Download_FastQ.sh
@@ -5,10 +5,9 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --job-name=download_fastq
 #SBATCH --time=24:00:00 # HH/MM/SS
-#SBATCH --mem=32G # memory requested, units available: K,M,G,T
+#SBATCH --mem=32G # units: K,M,G,T
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=your_account@med.cornell.edu
-
 
 wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRRXXX/XXX/SRRXXXXXXXX/SRRXXXXXXXX.fastq.gz
 wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRRXXX/XXX/SRRXXXXXXXX/SRRXXXXXXXX.fastq.gz
