797bea520ca9c017fecc75c39b73459ba5d89033
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-04-25 19:35:24 -0400

    Update multiqc_QC.sh

--- BulkRNASeq/multiqc_QC.sh
+++ BulkRNASeq/multiqc_QC.sh
@@ -1,2 +1,11 @@
 target_dir=/path/to/your_folder
 multiqc -o $target_dir $target_dir
+
+# In HPC, multiqc_AL.sh
+#!/bin/bash
+#SBATCH --job-name=multiqc
+#SBATCH --output=multiqc.out
+#SBATCH --partition=hpc_1,hpc_2,hpc_3
+
+export PATH="/scratch/lab/analysis_resources/tools:$PATH"
+multiqc -o $1 $1
