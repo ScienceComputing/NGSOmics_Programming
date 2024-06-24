3c87903fed6a06d615071a8d949980e0301dd80e
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-06-24 12:32:45 -0400

    Create Merge_CountMatrix_v2.py

---
+++ SingleCellRNASeq/Manipulate_Data/Merge_CountMatrix_v2.py
@@ -0,0 +1,25 @@
+# Reference: https://anndata.readthedocs.io/en/latest/concatenation.html
+
+import anndata as ad
+data_s1 = ad.read_h5ad('output_s1/counts_unfiltered/adata.h5ad')
+data_s2 = ad.read_h5ad('output_s2/counts_unfiltered/adata.h5ad')
+data_s1
+data_s2
+data_s1.X
+data_s2.X
+data_s1.obs.head() # Take a look at first few rows
+data_s2.obs.head()
+data_s1.var.head() # Take a look at first few columns
+data_s2.var.head()
+
+data_s12 = ad.concat({"data_s1": data_s1, "data_s2": data_s2}, join='outer', index_unique='-', label="data_origin")
+# The shared names of obs can be made unique by appending the relevant key using the index_unique argument
+
+data_s12.obs.head()
+#                          data_origin
+# barcode
+# AAACCTGAGAAACCAT-data_s1     data_s1
+# AAACCTGAGAAACCGC-data_s1     data_s1
+# AAACCTGAGAAACCTA-data_s1     data_s1
+# AAACCTGAGAAAGTGG-data_s1     data_s1
+# AAACCTGAGAACAATC-data_s1     data_s1
