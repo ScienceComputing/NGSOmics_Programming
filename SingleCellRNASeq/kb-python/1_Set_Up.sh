b7770a13c6f2f3b355d8a1463d3dbbcf86a69aaf
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-06-23 20:13:44 -0400

    Update 1_Set_Up.sh

--- SingleCellRNASeq/kb-python/1_Set_Up.sh
+++ SingleCellRNASeq/kb-python/1_Set_Up.sh
@@ -3,3 +3,5 @@ cd $work_dir/data/
 path_list = ["/Users/your_name/SRATool/sratoolkit.3.0.5-mac64/bin"]
 os.environ["PATH"] += os.pathsep + os.pathsep.join(path_list)
 print(os.environ.get("PATH"))
+
+pip install --quiet kb-python
