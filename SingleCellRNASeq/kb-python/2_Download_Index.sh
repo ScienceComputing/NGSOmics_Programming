3a90a55efcc4d948d4e705a4a0f4e63bb12d9de7
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-06-23 20:08:35 -0400

    Update 2_Download_Index.sh

--- SingleCellRNASeq/kb-python/2_Download_Index.sh
+++ SingleCellRNASeq/kb-python/2_Download_Index.sh
@@ -1,1 +1,4 @@
 kb ref -d mouse -i mouse.idx -g mouse_t2g.txt
+# Download a prebuilt reference for mouse
+# -i: name of the output index file
+# -g: name of the output transcript-to-gene mapping file
