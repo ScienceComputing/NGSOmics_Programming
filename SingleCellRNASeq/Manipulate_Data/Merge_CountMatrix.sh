7e686f8986d172177a489d7f246c1ac4f8415f42
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-06-24 11:20:57 -0400

    Update Merge_CountMatrix.sh

--- SingleCellRNASeq/Manipulate_Data/Merge_CountMatrix.sh
+++ SingleCellRNASeq/Manipulate_Data/Merge_CountMatrix.sh
@@ -22,5 +22,14 @@ aria2c -x 16 -s 16 https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-6108/iPSC_RGC
 # Quantify the read counts from paired-end FASTQ files
 # Single cell suspensions were loaded onto 10X Genomics Single Cell 3' Chips along with the reverse transcription master mix as per the manufacturer's protocol for the Chromium Single Cell 3' v2 Library (10X Genomics; PN-120233), to generate single cell gel beads in emulsion.
 kb ref -d human -i human_index.idx -g human_t2g.txt --verbose --kallisto /your_path/to/kallisto/build/src/kallisto
-kb count -i human_index.idx -g human_t2g.txt -x 10xv2 -o sample1 --h5ad -t 8 \
+kb count -i human_index.idx -g human_t2g.txt -x 10xv2 -o output_s1 --h5ad -t 8 \
 iPSC_RGCscRNAseq_Sample1_L005_R1.fastq.gz iPSC_RGCscRNAseq_Sample1_L005_R2.fastq.gz
+# -x: technology used for the sequencing; 10xv2 refers to the 10x Genomics Chromium Single Cell 3' v2 chemistry
+# -o: output directory where the results will be saved
+
+kb count -i human_index.idx -g human_t2g.txt -x 10xv2 -o output_s2 --h5ad -t 8 \
+iPSC_RGCscRNAseq_Sample2_L005_R1.fastq.gz iPSC_RGCscRNAseq_Sample2_L005_R2.fastq.gz
+
+# One-line code
+for sample in 1 2; do kb count -i human_index.idx -g human_t2g.txt -x 10xv2 -o output_s${sample} --h5ad -t 8 iPSC_RGCscRNAseq_Sample${sample}_L005_R1.fastq.gz iPSC_RGCscRNAseq_Sample${sample}_L005_R2.fastq.gz; done
+
