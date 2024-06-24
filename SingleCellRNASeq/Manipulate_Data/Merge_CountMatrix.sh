4302a07c04ba1b2113fefba7c6db6a4fa5918ad4
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-06-23 21:04:03 -0400

    Update Merge_CountMatrix.sh

--- SingleCellRNASeq/Manipulate_Data/Merge_CountMatrix.sh
+++ SingleCellRNASeq/Manipulate_Data/Merge_CountMatrix.sh
@@ -1,5 +1,6 @@
 # Reference: https://github.com/ScienceComputing/NGSOmics_Programming/tree/main/SingleCellRNASeq/kb-python
 # https://phoenixnap.com/kb/wget-command-with-examples
+# http://aria2.github.io/manual/en/html/aria2c.html#synopsis
 
 vim target_fastq.txt
 # https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-6108/iPSC_RGCscRNAseq_Sample1_L005_R1.fastq.gz
@@ -8,5 +9,13 @@ vim target_fastq.txt
 # https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-6108/iPSC_RGCscRNAseq_Sample2_L005_R2.fastq.gz
 wget -q -i target_fastq.txt
 # -i input file; -q: suppress output
+
+# A faster downloading of large files
+brew install aria2
+aria2c -x 16 -s 16 https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-6108/iPSC_RGCscRNAseq_Sample1_L005_R1.fastq.gz
+aria2c -x 16 -s 16 https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-6108/iPSC_RGCscRNAseq_Sample1_L005_R2.fastq.gz
+aria2c -x 16 -s 16 https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-6108/iPSC_RGCscRNAseq_Sample2_L005_R1.fastq.gz
+aria2c -x 16 -s 16 https://www.ebi.ac.uk/arrayexpress/files/E-MTAB-6108/iPSC_RGCscRNAseq_Sample2_L005_R2.fastq.gz
+
 
 
