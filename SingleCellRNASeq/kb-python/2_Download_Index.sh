1ebc6de9e01ef6d21e1ce14dfe16215e3ca5e686
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-06-23 21:13:26 -0400

    Update 2_Download_Index.sh

--- SingleCellRNASeq/kb-python/2_Download_Index.sh
+++ SingleCellRNASeq/kb-python/2_Download_Index.sh
@@ -4,4 +4,4 @@ kb ref -d mouse -i mouse.idx -g mouse_t2g.txt
 # -g: name of the output transcript-to-gene mapping file
 
 # kb ref -i index.idx -g t2g.txt -f1 transcriptome.fa genome.fa genome_annotation.gtf
-# -f1: name of the output transcriptome FASTA file derived from the input genome and annotation. 
+# -f1: name of the output transcriptome FASTA file derived from the input genome and annotation
