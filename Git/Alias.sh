4952c83e0de84580c726e662b3d0ce3b1687fe05
Anni <90419301+ScienceComputing@users.noreply.github.com>
2024-07-08 19:57:45 -0400

    Update Alias.sh

--- Git/Alias.sh
+++ Git/Alias.sh
@@ -1,5 +1,5 @@
 # Create an alias named ga for the `git add` command
-git config --global alias.ga add
+git config --global alias.ga add 
 ga scRNAseq.py
 
 # Create an alias named gc for the `git commit` command
